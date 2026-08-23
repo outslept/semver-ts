eb45e40492cc61fa05a75421ebe4b4f31d750b6d
outslept <135520429+outslept@users.noreply.github.com>
2026-08-23 11:24:44 +0300

    fix: Correct CheckNumeric branches

--- src/parser/core.ts
+++ src/parser/core.ts
@@ -1,30 +1,37 @@
-import type { IsNumericId, IsNumericIdLoose } from '../string'
-import type { ParserMode } from './mode'
+import type { IsNumericId, IsNumericIdLoose } from "../string";
+import type { ParserMode } from "./mode";
 
-type CheckNumeric<S extends string, M extends ParserMode> =
-  M extends 'loose' ? IsNumericIdLoose<S> extends true : IsNumericId<S> extends true;
+type CheckNumeric<S extends string, M extends ParserMode> = M extends "loose"
+  ? IsNumericIdLoose<S> extends true
+    ? true
+    : false
+  : IsNumericId<S> extends true
+    ? true
+    : false;
 
-export type ParseCore<S extends string, M extends ParserMode = 'strict'> =
-  S extends `${infer A}.${infer R1} `
+export type ParseCore<
+  S extends string,
+  M extends ParserMode = "strict",
+> = S extends `${infer A}.${infer R1} `
   ? R1 extends `${infer B}.${infer C} `
-  ? CheckNumeric<A, M> extends true
-  ? CheckNumeric<B, M> extends true
-  ? CheckNumeric<C, M> extends true
-  ? { major: A; minor: B; patch: C }
-  : never
-  : never
-  : never
-  : never
-  : never
+    ? CheckNumeric<A, M> extends true
+      ? CheckNumeric<B, M> extends true
+        ? CheckNumeric<C, M> extends true
+          ? { major: A; minor: B; patch: C }
+          : never
+        : never
+      : never
+    : never
+  : never;
 
-export type IsValidCore<S extends string, M extends ParserMode = 'strict'> =
-  ParseCore<S, M> extends never ? false : true
+export type IsValidCore<S extends string, M extends ParserMode = "strict"> =
+  ParseCore<S, M> extends never ? false : true;
 
-export type MajorOf<S extends string, M extends ParserMode = 'strict'> =
-  ParseCore<S, M> extends { major: infer X extends string } ? X : never
+export type MajorOf<S extends string, M extends ParserMode = "strict"> =
+  ParseCore<S, M> extends { major: infer X extends string } ? X : never;
 
-export type MinorOf<S extends string, M extends ParserMode = 'strict'> =
-  ParseCore<S, M> extends { minor: infer X extends string } ? X : never
+export type MinorOf<S extends string, M extends ParserMode = "strict"> =
+  ParseCore<S, M> extends { minor: infer X extends string } ? X : never;
 
-export type PatchOf<S extends string, M extends ParserMode = 'strict'> =
-  ParseCore<S, M> extends { patch: infer X extends string } ? X : never
+export type PatchOf<S extends string, M extends ParserMode = "strict"> =
+  ParseCore<S, M> extends { patch: infer X extends string } ? X : never;
